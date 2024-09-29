b7d70a2f5e1d552177b4a08cdad3c9cd8efba2fd
小萌新 <3209970865@qq.com>
2024-09-29 20:56:48 +0800

    Fix: translate error

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import ast
 import math
+import re
 import uuid
 from functools import cache
 
@@ -46,6 +48,19 @@ def from_curve_get_animation_offset(obj: bpy.types.Object, default=None) -> Vect
         except Exception as e:
             print("Curve use_path Error", e.args)
     return default
+
+
+def get_language_list() -> list:
+    """
+    Traceback (most recent call last):
+  File "<blender_console>", line 1, in <module>
+TypeError: bpy_struct: item.attr = val: enum "a" not found in ('DEFAULT', 'en_US', 'es', 'ja_JP', 'sk_SK', 'vi_VN', 'zh_HANS', 'ar_EG', 'de_DE', 'fr_FR', 'it_IT', 'ko_KR', 'pt_BR', 'pt_PT', 'ru_RU', 'uk_UA', 'zh_TW', 'ab', 'ca_AD', 'cs_CZ', 'eo', 'eu_EU', 'fa_IR', 'ha', 'he_IL', 'hi_IN', 'hr_HR', 'hu_HU', 'id_ID', 'ky_KG', 'nl_NL', 'pl_PL', 'sr_RS', 'sr_RS@latin', 'sv_SE', 'th_TH', 'tr_TR')
+    """
+    try:
+        bpy.context.preferences.view.language = ""
+    except TypeError as e:
+        matches = re.findall(r'\(([^()]*)\)', e.args[-1])
+        return ast.literal_eval(f"({matches[-1]})")
 
 
 class PublicData:
